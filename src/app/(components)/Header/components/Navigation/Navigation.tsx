c845ce895b6b4cdfee2ac608ae3b0cdea15619af
Microseversk <alexander.osipov.work@gmail.com>
2025-02-23 00:36:33 +0700

    develop 🦝 feature/header

--- src/app/(components)/Header/components/Navigation/Navigation.tsx
+++ src/app/(components)/Header/components/Navigation/Navigation.tsx
@@ -10,8 +10,8 @@ export const Navigation = () => {
 
   const navLinkClassName = (href: string) => {
     return cn(
-      "h-full flex items-center",
-      pathname === href ? "opacity-100 border-b-4 border-black" : "opacity-50",
+      "h-full flex items-center border-b-4 border-transparent",
+      pathname === href ? "opacity-100 border-black" : "opacity-50",
     );
   };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Cabin } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "./(components)/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const cabin = Cabin({
-  variable: "--font-cabin",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
   subsets: ["latin"],
 });
 
@@ -29,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body
-        className={`${cabin.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.className} antialiased`}>
         <Header />
         {children}
       </body>
