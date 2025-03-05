4ab5833e00e3095397fd7f958900bee4a8eb91d1
Microseversk <alexander.osipov.work@gmail.com>
2025-03-06 01:37:33 +0700

    Develop (#2)
    * develop 🦝 fix Jenkinsfile
    
    * develop 🦝 fix Dockerfile
    
    * develop 🦝 fix Dockerfile
    
    * develop 🦝 update instance

--- src/utils/api/instance.ts
+++ src/utils/api/instance.ts
@@ -3,3 +3,14 @@ import fetches from "@siberiacancode/fetches";
 export const instance = fetches.create({
   baseURL: process.env.API_URL || "http://localhost:4000/gateway",
 });
+
+instance.interceptors.request.use(
+  (config) => {
+    console.log(`------------------------------------------`);
+    console.log(`${config.method?.toUpperCase()} request on: ${config.url}`);
+    console.log(`Cache: ${config.next?.tags}`);
+    console.log(`------------------------------------------`);
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
