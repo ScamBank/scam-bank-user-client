e61af9103a50c2d8a6344aab398b5970fe4a184a
Microseversk <alexander.osipov.work@gmail.com>
2025-03-10 01:59:55 +0700

    master 🦝 ФИКСЫ ДЛЯ ДЕПЛОЯ

--- src/app/(components)/Header/components/LogoutButton/LogoutButton.tsx
+++ src/app/(components)/Header/components/LogoutButton/LogoutButton.tsx
@@ -6,9 +6,12 @@ import { useRouter } from "next/navigation";
 export const LogoutButton = () => {
   const router = useRouter();
   const onLogoutClick = async () => {
-    await fetch(`http://localhost:1337/api/logout`, {
-      method: "POST",
-    });
+    await fetch(
+      `${process.env.NEXT_PUBLIC_URL || "http://localhost:1337"}/api/logout`,
+      {
+        method: "POST",
+      },
+    );
     router.push("/login");
   };
 
